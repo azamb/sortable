e40932563d2b6f0ab324b2c05b529b0c7c1018c7
chuo06 <jzamb022@gmail.com>
2015-09-13 22:59:18 -0400

    WIP: First pass at building the results dictionary

--- matching.py
+++ matching.py
@@ -6,64 +6,70 @@
 # Some listings don't have a brand
 # Some product brands not in listings i.e: Contax
 
-# Maybe create index of listings by brand and cluster listings with same brands and similar titles
+# TODO stem/strip model numbers from dashes and other symbols
 
-# First pass - > check if manufacturers match
 import json
-# import codecs
+####################
+# Data Structures
+####################
+products = []
+listings = {}
+results = {}
 
 def read_source():
     '''
     TODO write docstring
     '''
-    products = []
-    listings = {}
-
     with open('products.txt') as products_file:
-    # with codecs.open('products.txt', encoding='ascii') as products_file:
         for product in products_file:
             products.append(json.loads(product))
 
     with open('listings.txt') as listings_file:
-    # with codecs.open('listings.txt', encoding='utf-8') as listings_file:
         for listing in listings_file:
             listing_dict = json.loads(listing)
-            manufacturer = listing_dict.get('manufacturer', 'brandless').lower()
+            manufacturer = listing_dict['manufacturer'].lower() or u'brandless'
+
             if manufacturer in listings:
                 listings[manufacturer].append(listing_dict)
             else:
                 listings[manufacturer] = [listing_dict]
 
-    from pudb import set_trace
-    set_trace()
     return products, listings
 
 def listing_is_product(listing, product):
     '''
     TODO write docstring
     '''
-    try:
-        if listing['manufacturer']:
-             if listing['manufacturer'].lower() != product['manufacturer'].lower():
-                return False
-             # manufacturers match
-             #
-             model, family = product['model'].lower(), product['family'].lower()
-             listing_title = listings['title'].lower()
-             if model in listing_title and family in listing_title:
-                return True
-             return False
-        else:
-            # no_manufacturer algo
-            print('This listing has no manufacturer information.')
-    except:
-        from pudb import set_trace
-        set_trace()
+    title = listing['title'].lower()
+    family = product.get('family', '').lower()
+    model = product['model'].lower()
+
+    if model in title:
+        if family and family in title:
+            return True
+    return False
+
+def find_results():
+    '''
+    TODO Write docstring
+    '''
+    for product in products:
+        _listings = listings.get(product['manufacturer'].lower(), [])
+        for _listing in _listings:
+            if listing_is_product(_listing, product):
+                if results.get(product['product_name']):
+                    results[product['product_name']].append(_listing)
+                else:
+                    results[product['product_name']] = [_listing]
+
+# def write_results():
+#     with open('results.json') as results_file:
 
 
 if __name__ == '__main__':
-    products, listings = read_source()
+    read_source()
+    find_results()
+    # write_results()
 
-
-    from pudb import set_trace
-    set_trace()
+    # from pudb import set_trace
+    # set_trace()

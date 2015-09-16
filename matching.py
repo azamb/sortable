db488308d0dce1212e1afad527eef870996ee59e
chuo06 <jzamb022@gmail.com>
2015-09-15 23:06:44 -0400

    WIP:     - pep8 and cleanup matching.py.     - Use codecs to read source files in the encoding chardet guessed.     - Write preliminary results to results.json. TODO:     - Better matching algorithm.

--- matching.py
+++ matching.py
@@ -1,45 +1,49 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-# product_name = $manufacturer_$family_$model
-# Some have family, some don't
-# Some listings don't have a brand
-# Some product brands not in listings i.e: Contax
-
-# TODO stem/strip model numbers from dashes and other symbols
-
 import json
-####################
-# Data Structures
-####################
+import codecs
+
 products = []
 listings = {}
-results = {}
+results = []
+
 
 def read_source():
+    '''Read source files.
+
+    Returns:
+        products (list): list of product dictionaries.
+        listings (dict): listings dictionary indexed by manufacturer.
     '''
-    TODO write docstring
-    '''
-    with open('products.txt') as products_file:
+    with codecs.open('products.txt', encoding='ascii') as products_file:
         for product in products_file:
             products.append(json.loads(product))
 
-    with open('listings.txt') as listings_file:
-        for listing in listings_file:
-            listing_dict = json.loads(listing)
-            manufacturer = listing_dict['manufacturer'].lower() or u'brandless'
+    with codecs.open('listings.txt', encoding='utf-8') as listings_file:
+        for _listing in listings_file:
+            listing = json.loads(_listing)
+            manufacturer = listing['manufacturer'].lower() or u'brandless'
 
             if manufacturer in listings:
-                listings[manufacturer].append(listing_dict)
+                listings[manufacturer].append(listing)
             else:
-                listings[manufacturer] = [listing_dict]
+                listings[manufacturer] = [listing]
 
     return products, listings
 
+
 def listing_is_product(listing, product):
+    '''Matching Algorithm.
+
+    Params:
+        listing (dict): listing from listings.txt
+        product (dict): product from products.txt
+
+    Returns:
+        bool: True if listing and product match, False otherwise.
     '''
-    TODO write docstring
-    '''
+    # TODO stemming and tokenizing title/family/model
+
     title = listing['title'].lower()
     family = product.get('family', '').lower()
     model = product['model'].lower()
@@ -49,27 +53,30 @@ def listing_is_product(listing, product):
             return True
     return False
 
+
 def find_results():
     '''
     TODO Write docstring
     '''
+    _results = {}
+
     for product in products:
         _listings = listings.get(product['manufacturer'].lower(), [])
         for _listing in _listings:
             if listing_is_product(_listing, product):
-                if results.get(product['product_name']):
-                    results[product['product_name']].append(_listing)
+                if _results.get(product['product_name']):
+                    _results[product['product_name']].append(_listing)
                 else:
-                    results[product['product_name']] = [_listing]
+                    _results[product['product_name']] = [_listing]
 
-# def write_results():
-#     with open('results.json') as results_file:
+    for product_name in _results:
+        results.append(
+            {'product_name': product_name, 'listings': _results[product_name]}
+        )
 
 
 if __name__ == '__main__':
     read_source()
     find_results()
-    # write_results()
-
-    # from pudb import set_trace
-    # set_trace()
+    with open('results.json', 'w') as results_file:
+        json.dump(results, results_file, indent=4)
